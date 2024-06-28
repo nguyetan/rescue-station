4765ba590d56aa87cdaa002782e25e266dd13b68
Linh Nguyễn <vanlinh14121@gmail.com>
2024-06-28 13:35:57 +0700

    ad unit test

---
+++ tests/lscp/requestTest.test.ts
@@ -0,0 +1,68 @@
+import { create } from 'apisauce';
+import test from 'ava';
+
+const requestSetup = {
+  firstStation: 1,
+  lastStation: 50,
+  numberStation: 10,
+};
+
+const api = create({
+  baseURL: 'http://localhost:5013',
+  withCredentials: true,
+});
+
+test('valid request to get LSCP', async (t) => {
+  const param = requestSetup;
+
+  const result = await api.post('/findLSCP', { data: param });
+  if (result.ok) {
+    const data = result.data as any;
+    t.deepEqual(Object.keys(data[0]).sort(), ['Id', 'FacilityPoints', 'XX', 'YY'].sort());
+
+    t.pass();
+  } else {
+    t.fail();
+  }
+});
+
+test('test valid type LSCP', async (t) => {
+  const param = requestSetup;
+
+  const result = await api.post('/findLSCP', { data: param });
+
+  if (result.ok) {
+    const data = result.data as any;
+    t.deepEqual(Object.keys(data[0]).sort(), ['Id', 'FacilityPoints', 'XX', 'YY'].sort());
+
+    const dataTest = data[0];
+    t.is(typeof dataTest.Id, 'number');
+    t.is(typeof dataTest.FacilityPoints, 'number');
+    t.is(typeof dataTest.XX, 'number');
+    t.is(typeof dataTest.YY, 'number');
+  } else {
+    t.fail();
+  }
+});
+
+test('test valid number poit result LSCP', async (t) => {
+  const param = requestSetup;
+  const result = await api.post('/findLSCP', { data: param });
+  if (result.ok) {
+    const data = result.data as any;
+    t.is(data.length, param.numberStation);
+  } else {
+    t.fail();
+  }
+});
+
+test('invalid request to get P-LSCP', async (t) => {
+  const param = requestSetup;
+
+  const result = await api.post('/findLSCP', { param });
+  if (result.ok) {
+    t.fail();
+  } else {
+    t.pass();
+  }
+});
