9d8f1e1e94932bed18cc9217a30c5115a6ee1ed6
Linh Nguyễn <linhnv1622@gmail.com>
2024-07-11 22:26:05 +0700

    update test case

--- tests/pCenter/requestTest.test.ts
+++ tests/pCenter/requestTest.test.ts
@@ -18,7 +18,7 @@ test('valid request to get P-Center', async (t) => {
   const result = await api.post('/findPCenter', { data: param });
   if (result.ok) {
     const data = result.data as any;
-    t.deepEqual(Object.keys(data[0]).sort(), ['Id', 'FacilityPoints', 'XX', 'YY'].sort());
+    t.deepEqual(Object.keys(data).sort(), ['selected', 'unselected'].sort());
 
     t.pass();
   } else {
@@ -33,13 +33,22 @@ test('test valid type PCenter', async (t) => {
 
   if (result.ok) {
     const data = result.data as any;
-    t.deepEqual(Object.keys(data[0]).sort(), ['Id', 'FacilityPoints', 'XX', 'YY'].sort());
+    const { selected, unselected } = data;
+    t.deepEqual(Object.keys(selected[0]).sort(), ['Id', 'FacilityPoints', 'XX', 'YY'].sort());
 
-    const dataTest = data[0];
-    t.is(typeof dataTest.Id, 'number');
-    t.is(typeof dataTest.FacilityPoints, 'number');
-    t.is(typeof dataTest.XX, 'number');
-    t.is(typeof dataTest.YY, 'number');
+    const pointSelected = selected[0];
+    t.is(typeof pointSelected.Id, 'number');
+    t.is(typeof pointSelected.FacilityPoints, 'number');
+    t.is(typeof pointSelected.XX, 'number');
+    t.is(typeof pointSelected.YY, 'number');
+
+    t.deepEqual(Object.keys(unselected[0]).sort(), ['Id', 'FacilityPoints', 'XX', 'YY'].sort());
+
+    const pointUnselected = unselected[0];
+    t.is(typeof pointUnselected.Id, 'number');
+    t.is(typeof pointUnselected.FacilityPoints, 'number');
+    t.is(typeof pointUnselected.XX, 'number');
+    t.is(typeof pointUnselected.YY, 'number');
   } else {
     t.fail();
   }
@@ -50,7 +59,7 @@ test('test valid number poit result PCenter', async (t) => {
   const result = await api.post('/findPCenter', { data: param });
   if (result.ok) {
     const data = result.data as any;
-    t.is(data.length, param.numberStation);
+    t.is(data.selected.length, param.numberStation);
   } else {
     t.fail();
   }
